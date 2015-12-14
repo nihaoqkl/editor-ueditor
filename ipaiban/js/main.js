c5158b085d09041cc39ae12462f100976b8e4349
work-PC <1187328898@qq.com>
2015-12-14 18:33:30 +0800

    20151214

---
+++ ipaiban/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * 编辑器总入口文件
+ * Created by qkl | QQ:80508567 Wechat:qklandy on 2015/12/14.
+ */
+define(function(require, exports, module) {
+
+    var $=require('jquery');
+
+    console.log(wxqqEditor);
+
+
+});
---
+++ ipaiban/js/seajs-config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by qkl | QQ:80508567 Wechat:qklandy on 2015/12/14.
+ */
+seajs.config({
+    base: "./js/spm_modules/",
+    alias: {
+        "jquery": "jquery/1.11.2/jquery.min.js",
+        "bootstrap.css":"http://cdn.bootcss.com/bootstrap/3.3.5/css/bootstrap.min.css"
+    }
+});
